092e77a630b16ed45e94255bd222430e2826a774
KjellSchool <kjell.elslander@student.howest.be>
2026-06-02 10:11:46 +0200

    database test

---
+++ dev-tests/mapbox-test/app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+import { Map } from "../components/map";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="map-container">
+      <Map />
+    </div>
+  );
+}
