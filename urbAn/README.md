3b78bf486aaaaffc794d2f9fcb7873420086f880
KjellSchool <kjell.elslander@student.howest.be>
2026-06-04 05:59:05 +0200

    database connection + base function per table

--- urbAn/README.md
+++ urbAn/README.md
@@ -4,4 +4,6 @@
 
 ### Kjell
 
-- Set up the project folder (React Router)
+- Set up the project folder (React Router).
+- Made changes to the database setup.
+- Added the connection to the database and wrote basic functions for all of the tables.
---
+++ urbAn/app/database/locations.js
@@ -0,0 +1,16 @@
+import { supabase } from "./supabase";
+
+export const getLocations = async () => {
+  const { data, error } = await supabase.from("locations").select("*");
+
+  return { data, error };
+};
+
+export const getLocation = async (locationId) => {
+  const { data, error } = await supabase
+    .from("locations")
+    .select("*")
+    .eq("location_id", locationId);
+
+  return { date, error };
+};
