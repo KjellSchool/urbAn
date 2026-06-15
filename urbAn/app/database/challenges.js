0baf55e1c0e67376ad90c7e4d451ea1ccbc4b404
KjellSchool <kjell.elslander@student.howest.be>
2026-06-15 14:53:13 +0200

    profile achievements

--- urbAn/app/database/challenges.js
+++ urbAn/app/database/challenges.js
@@ -10,7 +10,18 @@ export const getChallenge = async (challengeId) => {
   const { data, error } = await supabase
     .from("challenges")
     .select("*")
-    .eq("challenge_id", challengeId);
+    .eq("challenge_id", challengeId)
+    .single();
+
+  return { data, error };
+};
+
+export const getCompletedChallenges = async (profileId) => {
+  const { data, error } = await supabase
+    .from("challenge_progress")
+    .select("*")
+    .eq("profile_id", profileId)
+    .eq("completed", "TRUE");
 
   return { data, error };
 };
