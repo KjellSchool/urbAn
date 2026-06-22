70d315a4f054130647ff6ac200b55210cc5f4cb4
goossens <saigita.goossens@student.howest.be>
2026-06-22 10:07:42 +0200

    empty state works

--- urbAn/app/routes/home.tsx
+++ urbAn/app/routes/home.tsx
@@ -76,8 +76,8 @@ const Home = () => {
     const a =
       Math.sin(dLat / 2) ** 2 +
       Math.cos((lat1 * Math.PI) / 180) *
-        Math.cos((lat2 * Math.PI) / 180) *
-        Math.sin(dLon / 2) ** 2;
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) ** 2;
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
